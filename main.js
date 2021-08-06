2bc4dfab494c88304b45fd2b25eaaacff2bb8a65
Aman Kumar Sahoo <60147674+silveraman@users.noreply.github.com>
2021-08-06 10:20:49 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,17 @@
+
+const toggle = document.querySelector(".menu-toggle");
+const siteNav = document.querySelector(".site-nav");
+
+toggle.addEventListener("click", function(){
+    if(siteNav.classList.contains("site-nav--open")) {
+      siteNav.classList.remove("site-nav--open");
+      toggle.classList.remove("open");
+
+      siteNav.classList.add("site-nav--closed");
+    } else{
+        siteNav.classList.add("site-nav--closed");
+        toggle.classList.add("open");
+
+        siteNav.classList.add("site-nav--open");
+    }
+});
